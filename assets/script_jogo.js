8bed9475555ff022e6b30627e7e2b204538b8f0f
Matheus Ramos Marcolino <80594518+theusrmx@users.noreply.github.com>
2023-05-08 17:42:10 -0300

    Add files via upload

---
+++ assets/script_jogo.js
@@ -0,0 +1,433 @@
+﻿var carta1 = {
+    nome: "Homem de Ferro",
+    imagem: "https://static.wikia.nocookie.net/disney/images/3/35/IronMan-EndgameProfile.jpg/revision/latest?cb=20200516075827&path-prefix=pt-br",
+
+    atributos: {
+    tecnologia: 10,
+    combate: 8,
+    agilidade: 1,
+    magia: 0
+  }
+
+};
+
+var carta2 = {
+    nome: "Hulk",
+    imagem: "https://i.pinimg.com/originals/c8/68/5e/c8685e46c6b2aa9cd9603e3307b07592.jpg",
+
+    atributos: {
+    tecnologia: 5,
+    combate: 10,
+    agilidade: 0,
+    magia: 0
+  }
+
+};
+
+var carta3 = {
+    nome: "Thor",
+    imagem: "https://static.wikia.nocookie.net/universocinematograficomarvel/images/4/4c/Thor-Ultimato.jpg/revision/latest?cb=20190514112708&path-prefix=pt",
+
+    atributos: {
+    tecnologia: 0,
+    combate: 8,
+    agilidade: 0,
+    magia: 8
+  }
+
+};
+
+var carta4 = {
+    nome: "Capitão América",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzpaYh2159R0WfhgYzzgj_iEG_GDim2k_4GnjLGGvgTaEJuszoa8Ul60UfZKfv4U4Utt0&usqp=CAU",
+
+    atributos: {
+    tecnologia: 5,
+    combate: 10,
+    agilidade: 8,
+    magia: 0
+  }
+
+};
+
+var carta5 = {
+    nome: "Viúva Negra",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/846a9086-8a40-43e0-aa10-2fc7d6d73730/dejez2j-7a8b06b3-b9fa-4291-979c-427a5d26fa78.jpg/v1/fill/w_1280,h_1920,q_75,strp/black_widow__2021__natasha_romanoff_textless_by_mintmovi3_dejez2j-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTkyMCIsInBhdGgiOiJcL2ZcLzg0NmE5MDg2LThhNDAtNDNlMC1hYTEwLTJmYzdkNmQ3MzczMFwvZGVqZXoyai03YThiMDZiMy1iOWZhLTQyOTEtOTc5Yy00MjdhNWQyNmZhNzguanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.0zqLTCOvTt9tXoZDfK3aoyDginZNvETe3OAQT0WZXA8",
+
+    atributos: {
+    tecnologia: 7,
+    combate: 10,
+    agilidade: 10,
+    magia: 0
+  }
+
+};
+
+var carta6 = {
+    nome: "Gavião Arqueiro",
+    imagem: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/f/f5/Hawkeye_Char_Poster.jpg/revision/latest?cb=20211118181820",
+
+    atributos: {
+    tecnologia: 9,
+    combate: 8,
+    agilidade: 7,
+    magia: 0
+  }
+
+};
+
+var carta7 = {
+    nome: "Senhor das Estrelas",
+    imagem: "https://static.wikia.nocookie.net/universocinematograficomarvel/images/9/96/Star-Lord_AIW_Profile.jpg/revision/latest?cb=20200525185429&path-prefix=pt",
+
+    atributos: {
+    tecnologia: 9,
+    combate: 5,
+    agilidade: 3,
+    magia: 0
+  }
+
+};
+
+var carta8 = {
+    nome: "Gamora",
+    imagem: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/6/61/Gamora_AIW_Profile.jpg/revision/latest?cb=20180518212221",
+
+    atributos: {
+    tecnologia: 7,
+    combate: 9,
+    agilidade: 5,
+    magia: 0
+  }
+
+};
+
+var carta9 = {
+    nome: "Rocket & Groot",
+    imagem: "https://s1.1zoom.me/big0/105/Raccoons_Guardians_of_the_Galaxy_Vol._2_Rocket_528516_1235x1024.jpg",
+
+    atributos: {
+    tecnologia: 10,
+    combate: 9,
+    agilidade: 5,
+    magia: 0
+  }
+
+};
+
+var carta10 = {
+    nome: "Drax",
+    imagem: "https://static.wikia.nocookie.net/marvel/images/4/44/Drax_Profile%281%29.jpeg/revision/latest?cb=20170406220453&path-prefix=pt-br",
+
+    atributos: {
+    tecnologia: 1,
+    combate: 8,
+    agilidade: 0,
+    magia: 0
+  }
+
+};
+
+var carta10 = {
+    nome: "Feiticeira Escalarte",
+    imagem: "https://static.wikia.nocookie.net/marvelmovies/images/2/25/Wanda_Maximoff_in_her_Scarlet_Witch_coustme.jpg/revision/latest?cb=20211226003712",
+
+    atributos: {
+    tecnologia: 0,
+    combate: 8,
+    agilidade: 2,
+    magia: 10
+  }
+
+};
+
+var carta11 = {
+    nome: "Visão",
+    imagem: "https://static.wikia.nocookie.net/universocinematograficomarvel/images/2/2f/Vision_AIW_Profile.jpg/revision/latest/scale-to-width-down/2000?cb=20190117220536&path-prefix=pt",
+
+    atributos: {
+    tecnologia: 10,
+    combate: 8,
+    agilidade: 3,
+    magia: 5
+  }
+
+};
+
+var carta12 = {
+    nome: "Homem-Formiga",
+    imagem: "https://assets.mycast.io/characters/scott-lang-241399-normal.jpg?1589141311",
+
+    atributos: {
+    tecnologia: 10,
+    combate: 7,
+    agilidade: 4,
+    magia: 0
+  }
+
+};
+
+var carta13 = {
+    nome: "Vespa",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/846a9086-8a40-43e0-aa10-2fc7d6d73730/dcgj8b9-242d2d61-de7a-4cba-be48-c9de607a1030.png/v1/fill/w_800,h_1185,q_80,strp/ant_man_and_the_wasp_textless_poster__hope_by_mintmovi3_dcgj8b9-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTE4NSIsInBhdGgiOiJcL2ZcLzg0NmE5MDg2LThhNDAtNDNlMC1hYTEwLTJmYzdkNmQ3MzczMFwvZGNnajhiOS0yNDJkMmQ2MS1kZTdhLTRjYmEtYmU0OC1jOWRlNjA3YTEwMzAucG5nIiwid2lkdGgiOiI8PTgwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.GiqjNqQXy6bo9TGSQKCVwq_AkSbW5yr1gzxthxpQdD8",
+
+    atributos: {
+    tecnologia: 10,
+    combate: 8,
+    agilidade: 5,
+    magia: 0
+  }
+
+};
+
+var carta14 = {
+    nome: "Doutor Estranho",
+    imagem: "https://pbs.twimg.com/media/Ds5TnsXUwAAe6OD?format=jpg&name=large",
+
+    atributos: {
+    tecnologia: 0,
+    combate: 8,
+    agilidade: 4,
+    magia: 10
+  }
+
+};
+
+var carta14 = {
+    nome: "Homem-Aranha",
+    imagem: "https://preview.redd.it/q2c8ylp6gby71.jpg?auto=webp&s=7a9ae2194215b81293517db8fb568f3d05c7ff09",
+
+    atributos: {
+    tecnologia: 5,
+    combate: 9,
+    agilidade: 9,
+    magia: 0
+  }
+
+};
+
+var carta15 = {
+    nome: "Pantera Negra",
+    imagem: "https://i.pinimg.com/originals/8a/a1/69/8aa1695bbdc275ef844b58c58bd486c0.png",
+
+    atributos: {
+    tecnologia: 9,
+    combate: 10,
+    agilidade: 9,
+    magia: 4
+  }
+
+};
+
+var carta16 = {
+    nome: "Capitã Marvel",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/869ae92e-855f-4458-8e52-e9d04604a682/dcx93fy-ec340f41-999e-4fb9-b7d5-22aab69cd858.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg2OWFlOTJlLTg1NWYtNDQ1OC04ZTUyLWU5ZDA0NjA0YTY4MlwvZGN4OTNmeS1lYzM0MGY0MS05OTllLTRmYjktYjdkNS0yMmFhYjY5Y2Q4NTguanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.rObtN4ISTYQ7sdy9bsbco0llh7OlHA_zr1wDBFWO9rY",
+
+    atributos: {
+    tecnologia: 0,
+    combate: 9,
+    agilidade: 10,
+    magia: 3
+  }
+
+};
+
+
+var carta17 = {
+    nome: "Capitão América (Sam Wilson)",
+    imagem: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/1/1b/SamWilson-CaptainAmerica.png/revision/latest?cb=20210524144921",
+
+    atributos: {
+    tecnologia: 6,
+    combate: 9,
+    agilidade: 8,
+    magia: 0
+  }
+
+};
+
+var carta18 = {
+    nome: "Soldado Invernal",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b70af2b5-d696-4bf5-b0d9-4cfbfc5b8bc4/defk8o1-ba8b813e-e6a6-4731-8a10-ea41fb5e4f8a.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2I3MGFmMmI1LWQ2OTYtNGJmNS1iMGQ5LTRjZmJmYzViOGJjNFwvZGVmazhvMS1iYThiODEzZS1lNmE2LTQ3MzEtOGExMC1lYTQxZmI1ZTRmOGEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.hFlUDznopY_qB0XjlkM_a3niRGab_2eauMhn60Kg6b8",
+
+    atributos: {
+    tecnologia: 4,
+    combate: 8,
+    agilidade: 4,
+    magia: 0
+  }
+
+};
+
+var carta19 = {
+    nome: "Loki",
+    imagem: "https://preview.redd.it/3yb43m45m0o61.jpg?width=1687&format=pjpg&auto=webp&s=8b7481caea2f62cd8c400a0412b03a223e185627",
+
+    atributos: {
+    tecnologia: 0,
+    combate: 4,
+    agilidade: 2,
+    magia: 8
+  }
+
+};
+
+var carta19 = {
+    nome: "Sylvie",
+    imagem: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/a/a3/The_Variant_Loki.png/revision/latest?cb=20210803190619",
+
+    atributos: {
+    tecnologia: 0,
+    combate: 5,
+    agilidade: 3,
+    magia: 9
+  }
+
+};
+
+var carta20 = {
+    nome: "Yelena Belova",
+    imagem: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/e/e4/Hawkeye_-_Yelena_Belova.jpg/revision/latest?cb=20211225191951",
+
+    atributos: {
+    tecnologia: 6,
+    combate: 8,
+    agilidade: 7,
+    magia: 0
+  }
+
+};
+
+var carta21 = {
+    nome: "Kate Bishop",
+    imagem: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/3/31/Kate_Bishop_Character_Poster.jpg/revision/latest?cb=20211120143826",
+
+    atributos: {
+    tecnologia: 5,
+    combate: 5,
+    agilidade: 6,
+    magia: 0
+  }
+
+};
+
+var carta22 = {
+    nome: "Shang-Chi",
+    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/846a9086-8a40-43e0-aa10-2fc7d6d73730/dep1gjh-df450376-fa01-4b95-8d40-f634188b803d.jpg/v1/fill/w_741,h_1079,q_70,strp/shang_chi__2021__poster_textless__2_by_mintmovi3_dep1gjh-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTg2MyIsInBhdGgiOiJcL2ZcLzg0NmE5MDg2LThhNDAtNDNlMC1hYTEwLTJmYzdkNmQ3MzczMFwvZGVwMWdqaC1kZjQ1MDM3Ni1mYTAxLTRiOTUtOGQ0MC1mNjM0MTg4YjgwM2QuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.E9QkeRT3y7mPbALTXAONPpi_iMhx2N8W_-ZQHHidMPc",
+
+    atributos: {
+    tecnologia: 0,
+    combate: 9,
+    agilidade: 6,
+    magia: 8
+  }
+
+};
+
+var carta23 = {
+    nome: "Sersi",
+    imagem: "https://static.wikia.nocookie.net/marveldatabase/images/5/54/Eternals_%28film%29_poster_003_textless.jpg/revision/latest?cb=20211109172551",
+
+    atributos: {
+    tecnologia: 9,
+    combate: 1,
+    agilidade: 1,
+    magia: 5
+  }
+
+};
+
+var carta23 = {
+    nome: "Ikaris",
+    imagem: "https://pbs.twimg.com/media/FBb48GnVIAAntJS?format=jpg&name=4096x4096",
+
+    atributos: {
+    tecnologia: 9,
+    combate: 8,
+    agilidade: 6,
+    magia: 6
+  }
+
+};
+
+var carta24 = {
+    nome: "O Vigia",
+    imagem: "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/e/e5/Uatu_the_Watcher.png/revision/latest?cb=20210801041348",
+
+    atributos: {
+    tecnologia: 0,
+    combate: 2,
+    agilidade: 2,
+    magia: 10
+  }
+
+};
+
+var carta25 = {
+    nome: "Valquíria",
+    imagem: "https://i.pinimg.com/736x/1e/94/28/1e94282438d8dd74aa27f3cbe73036e9.jpg",
+
+    atributos: {
+    tecnologia: 0,
+    combate: 9,
+    agilidade: 5,
+    magia: 4
+  }
+
+};
+
+var carta26 = {
+    nome: "Thanos",
+    imagem: "https://static.wikia.nocookie.net/marvel/images/b/bc/Thanos_%28Earth-199999%29_from_Avengers_Infinity_War_-_The_Official_Movie_Special_001.jpg/revision/latest?cb=20190503185709&path-prefix=pt-br",
+
+    atributos: {
+    tecnologia: 10,
+    combate: 10,
+    agilidade: 10,
+    magia: 10
+  }
+
+};
+
+
+/*functions*/
+
+var cartas_jogo = [
+    carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8,
+    carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16,
+    carta17, carta18, carta19, carta20, carta21, carta22, carta23, carta24,
+    carta25, carta26
+];
+
+var cartas_jogador;
+
+var cartas_maquina;
+
+
+/*function embaralharCartas(arr) {
+    // Loop em todos os elementos
+    for (let i = arr.length - 1; i > 0; i--) {
+        // Escolhendo elemento aleatório
+        const j = Math.floor(Math.random() * (i + 1));
+    // Reposicionando elemento
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+// Retornando array com aleatoriedade
+return arr;
+}
+
+function distribuirCartas(){
+    embaralharCartas(cartas_jogo);
+
+
+    cartas_jogador = cartas_jogo.slice(0,13);
+    cartas_maquina = cartas_jogo.slice(13,26);
+
+    console.log(cartas_jogador);
+    console.log(cartas_maquina);
+
+    
+}
+
+distribuirCartas();*/
+
